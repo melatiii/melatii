9e62b9b027c154ccfbc6424f483bdf2ca26e5632
melatiii <123279479+melatiii@users.noreply.github.com>
2023-09-08 15:57:42 +0700

    Add files via upload

---
+++ #Tugas pelatihan coding menghitung konve.py
@@ -0,0 +1,28 @@
+#Tugas pelatihan coding menghitung konversi suhu
+
+#Menghitung Konfersi suhu celcius ke dalam fahrenheit
+
+celcius = float(input('masukkan suhu ke dalam celcius :'))
+print ("Suhu yaitu", celcius, "celcius")
+
+#Fahrenheit
+fahrenheit = ((9/5)*celcius)+32
+print ("Suhu dalam fahrenheit yaitu", fahrenheit, "fahrenheit")
+ 
+#Menghitung konversi suhu fahrenheit ke dalam celcius
+
+fahrenheit = float(input('Masukkan suhu ke dalam fahrenheit :'))
+print ("Suhu yaitu", fahrenheit, "fahrenheit")
+
+#Celcius
+celcius = ((5/9)*fahrenheit)-32
+print ("Suhu dalam celcius yaitu", celcius, "celcius")
+
+#Menghitung konversi suhu kelvin ke dalam celcius
+
+kelvin = float(input('Masukkan suhu ke dalam kelvin :'))
+print ("Suhu yaitu", kelvin, "kelvin")
+
+#Celcius
+celcius = kelvin-273
+print ("Suhu dalam celcius yaitu", celcius, "celcius") 
